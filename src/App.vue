94fb23ba43a33c0c9a3e1c72cd785308f29aa1ae
Brandon Apol <brandonapol@cedarville.edu>
2023-03-02 10:38:00 -0500

    created form

--- src/App.vue
+++ src/App.vue
@@ -1,150 +1,47 @@
 <template>
-  <div class="main">
-      <section class="bg-cover bg-center hero">
-        <div class="dark-overlay"></div>
-        <div class="position-relative z-index-1">
-          <div class="container text-center text-white">
-            <p class="font-italic lead"><i class="far fa-envelope"></i><a class="leavemealone" href="mailto:apolbrandon@gmail.com"> apolbrandon@gmail.com</a> | <i class="fas fa-phone"></i> (937)380-8954 | <i class="fab fa-github"></i> <a class="leavemealone" href="https://github.com/brandonapol">github.com/brandonapol</a> | <i class="fab fa-linkedin-in"></i><a href="https://www.linkedin.com/in/brandonapol/" class="leavemealone"> Brandon Apol</a></p>
-            <h1 class="text-uppercase my-4">
-              Brandon Apol
-            </h1>
-            <p class="font-italic lead"><i class="fas fa-map-marker-alt"></i> Fairbanks, Alaska</p>
-            <p class="font-italic lead">Educator, Learner, Programmer, Musician</p>
-          </div>
-        </div>
-        <div class="scroll-btn link-scroll"><i class="fas fa-angle-double-down"></i></div>
-      </section>
-      <section id="1">
-        <div class="d-flex h-100 align-items-center">
-          <div class="container">
-            <div class="row align-items-center">
-              <div class="col-lg-6 mb-4 mb-lg-0">
-                <header class="text-center">
-                  <h2 class="text-uppercase lined">About Me</h2>
-                </header>
-                <p class="lead">I love programming.</p>
-                
-                <p>I am enthusiastic about both programming itself, and teaching others about it. I love learning and spend several hours a day just learning new things as I am able. Recently, I have been fascinated with functional programming instead of object oriented, and learning about how compilers work.</p>
-              </div>
-              <div class="col-lg-6"><img src="../static/images/IMG_1199 copy.jpeg" alt="Profile photo" class="img-fluid rounded-circle d-block mx-auto" id="profilephoto"></div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="bg-gray">
-        <div class="d-flex h-100 align-items-center">
-          <div class="container">
-            <header class="mb-5 text-center">
-              <h2 class="text-uppercase lined">Skills</h2>
-            </header>
-            <div class="row text-center">
-              <div class="col-lg-4 col-md-6 mb-4">
-                <div class="icon mb-3"><i class="fas fa-desktop"></i></div>
-                <h4 class="text-uppercase lined lined-compact">Web design</h4>
-                <p class="text-mted">HTML, CSS, JavaScript, React, Bootstrap, TypeScript with style!</p>
-              </div>
-              <div class="col-lg-4 col-md-6 mb-4">
-                <div class="icon mb-3"><i class="fas fa-server"></i></div>
-                <h4 class="text-uppercase lined lined-compact">Back End</h4>
-                <p class="text-mted">Python, Flask, Java, Spring, Django, FastAPI</p>
-              </div>
-              <div class="col-lg-4 col-md-6 mb-4">
-                <div class="icon mb-3"><i class="fas fa-terminal"></i></div>
-                <h4 class="text-uppercase lined lined-compact">DevOps</h4>
-                <p class="text-mted">AWS CCP, expected AWS Certified Developer July 2022, Docker, Git, ECS, EC2, S3, DigitalOcean, Heroku</p>
-              </div>
-              <div class="col-lg-4 col-md-6 mb-4">
-                <div class="icon mb-3"><i class="fas fa-school"></i></div>
-                <h4 class="text-uppercase lined lined-compact">Teaching</h4>
-                <p class="text-mted">Taught lessons, small groups, large classrooms and more. Licensed public school teacher with proven professionalism, communication, and decision making experience</p>
-              </div>
-              <div class="col-lg-4 col-md-6 mb-4">
-                <div class="icon mb-3"><i class="fas fa-database"></i></div>
-                <h4 class="text-uppercase lined lined-compact">Data</h4>
-                <p class="text-mted">SQL, Pandas and Numpy for Python, R, Regex, ORMs</p>
-              </div>
-              <div class="col-lg-4 col-md-6 mb-4">
-                <div class="icon mb-3"><i class="fas fa-lightbulb"></i></div>
-                <h4 class="text-uppercase lined lined-compact">Creative thinker</h4>
-                <p class="text-mted">Backgrounds in music, education, customer service, outdoorsmanship, youth development, and more means I bring more to the table than just software development</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section>
-        <div class="d-flex h-100 align-items-center">
-          <div class="container">
-            <header class="text-center mb-5">
-              <h2 class="text-uppercase lined">Highlighted Work</h2>
-            </header>
-            <div class="row">
-              <div class="col-lg-12 text-center">
-                <p><a href="https://github.com/brandonapol/">My Github</a></p>
-                <p><a href="https://github.com/brandonapol/recommendation-letter">Letter of Recommendation</a></p>
-              </div>
-
-            <div class="column">
-                <h4><a href="https://github.com/brandonapol/Final-Project-API-API">Flask App Generator</a></h4>
-                <p>This project was my response to observing that most Flask apps were basically identical, so I made a full stack app that zipped a template Flask app and downloaded it. It's saved me more time than I even thought it would.</p>
-                <div class="m-10"></div><br>
-                <h4><a href="codingtemple.com/self-paced/">Coding Temple: Self-Paced Course</a></h4>
-                <p>Over the course of two months, I created and delivered a self-paced version of Coding Temple's full-time live bootcamp course, which delivered a return on investment of over 30:1. I tutored students in code for the following 7 months while building other projects for the company.</p>
-                <div class="m-10"></div><br>
-                <h4><a href="#">Coding Temple: Self-Paced Data Course</a></h4>
-                <p>In-process: Creating and delivering a brand-new Data Analysis self-paced course for Coding Temple. Learned new technologies and made educational content for each technology learned.</p>
-            </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="bg-gray">
-        <div class="d-flex h-100 align-items-center">
-          <div class="container">
-            <header class="text-center mb-5">
-              <h2 class="text-uppercase lined">Other Stuff About Me</h2>
-            </header>
-            <div class="row">
-              <div class="col-lg-6">
-                <iframe src="https://open.spotify.com/embed/album/2IoqqeZojxqSfJo4ll688L" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-              </div>
-              <div class="col-lg-6">
-                <p>I have a lot of interests! I do a lot of stuff!</p>
-                <p>I enjoy rock climbing, music theory, cross country skiing, spending time with my wife, learning about functional programming, swanky coffee, farming, mountaineering (and generally being on top of things), bicycling, ice fishing, church history, books by Dietrich Bonhoeffer, and more!</p>
-                <p>I also was a music teacher and I still play music and teach for fun! On the left is an album I co-produced while I was in college, and I hope you give it a listen.</p>
-                <p></p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section>
-        <div class="d-flex h-100 align-items-center">
-          <div class="container">
-            <header class="text-center mb-5">
-              <h2 class="text-uppercase lined">Find Me</h2>
-            </header>
-            <!-- <div class="row"> -->
-              <div class="col-lg-12">
-                <ul class="mb-0 list-inline text-center">
-                  <li class="list-inline-item"><a href="https://www.facebook.com/bapol1/" class="social-link social-link-facebook"><i class="fab fa-facebook-f"></i></a></li>
-                  <li class="list-inline-item"><a href="https://twitter.com/_BrandonApol" class="social-link social-link-twitter"><i class="fab fa-twitter"></i></a></li>
-                  <li class="list-inline-item"><a href="https://github.com/brandonapol" class="social-link social-link-github"><i class="fab fa-github"></i></a></li>
-                  <li class="list-inline-item"><a href="https://www.instagram.com/brandonapol/" class="social-link social-link-instagram"><i class="fab fa-instagram"></i></a></li>
-                  <li class="list-inline-item"><a href="mailto: brandonapol@cedarville.edu" class="social-link social-link-email"><i class="fas fa-envelope"></i></a></li>
-                  <li class="list-inline-item"><a href="https://calendly.com/brandonapol/30min" class="social-link social-link-calendly"><i class="far fa-calendar-alt"></i></a></li>
-                </ul>
-              </div>
-            <!-- </div> -->
-            <footer class="py-5 mt-5">
-              <div class="row">
-                <div class="text-center">
-                  <p class="font-italic mb-0 text-gray">&copy; Brandon Apol</p>
-                </div>
-              </div>
-            </footer>
-          </div>
-        </div>
-      </section>
-  </div>
+  <form>
+    <label>
+      Title:
+      <input v-model="title" type="text" />
+    </label>
+    <label>
+      Author:
+      <input v-model="author" type="text" />
+    </label>
+    <label>
+      Content:
+      <textarea v-model="content"></textarea>
+    </label>
+    <button @click.prevent="handleSubmit">Submit</button>
+  </form>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const title = ref('');
+    const author = ref('');
+    const content = ref('');
+
+    function handleSubmit() {
+      const formData = {
+        title: title.value,
+        author: author.value,
+        content: content.value,
+      };
+      console.log(formData); 
+      // TODO: send to server
+    }
+
+    return {
+      title,
+      author,
+      content,
+      handleSubmit,
+    };
+  },
+});
+</script>
+
