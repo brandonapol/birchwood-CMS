f73822a53c985aa23576b6bed902ad5118f91a41
Brandon Apol <brandonapol@cedarville.edu>
2023-03-04 19:56:52 -0500

    auth done

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createAuth0, Auth0VueClientOptions } from '@auth0/auth0-vue'
+import router from './router/router'
 
-createApp(App).mount('#app')
+interface ClientAuth extends Auth0VueClientOptions {
+    redirect_uri: any;
+}
+
+const options: ClientAuth = {
+    domain: "dev-626lcx07uwy8kjn5.us.auth0.com",
+    clientId: "SKPxrOE9dTea8U2zNFczclicNGzPg6Jw",
+    redirect_uri: window.location.origin
+}
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(
+    createAuth0(options)
+);
+
+app.mount('#app')
---
+++ src/router/router.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import { authGuard } from '@auth0/auth0-vue';
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            component: Home,
+            meta: {
+                title: 'Birchwood CMS'
+            },
+            beforeEnter: authGuard
+        }
+    ]
+})
+
+export default router
